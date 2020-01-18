79e8be7e1442c37e0ccb45b317bbad2a813f8c44
MRomario <masorinwork@gmail.com>
2020-01-18 13:29:50 +0300

    created authentication: module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+    AuthenticationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
